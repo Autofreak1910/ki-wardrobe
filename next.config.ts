3558d3bf76012bfb3b360a97a2fc3ed3fe5605bc
Luca Darvas <darvas.luca.1@gmail.com>
2026-06-14 14:01:41 +0200

    fix: cache pages, no reload on tab switch

--- next.config.ts
+++ next.config.ts
@@ -3,23 +3,12 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 export default withNextIntl({
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0',
-          },
-        ],
-      },
-    ]
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+      static: 180,
+    },
   },
 })
