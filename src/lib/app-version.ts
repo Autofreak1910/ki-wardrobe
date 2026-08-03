d39737c2b566abd94e52076baadcc08930377fff
Luca Darvas <darvas.luca.1@gmail.com>
2026-08-03 18:37:47 +0200

    Add app-version config

---
+++ src/lib/app-version.ts
@@ -0,0 +1,25 @@
+﻿export const CURRENT_VERSION = '1.0.0'
+
+export type ChangelogEntry = {
+  version: string
+  date: string
+  notesDe: string[]
+  notesEn: string[]
+}
+
+export const CHANGELOG: ChangelogEntry[] = [
+  {
+    version: '1.0.0',
+    date: '2026-08-03',
+    notesDe: [
+      'Erste Version des Update-Systems',
+    ],
+    notesEn: [
+      'First version of the update system',
+    ],
+  },
+]
+
+export function getLatestChangelogEntry(): ChangelogEntry {
+  return CHANGELOG[0]
+}
