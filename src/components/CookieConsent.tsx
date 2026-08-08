e160f44add863e8b70c455c35a07cbb798c6fb89
Luca Darvas <darvas.luca.1@gmail.com>
2026-08-08 13:05:37 +0200

    Test version bump

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -36,7 +36,7 @@ export default function CookieConsent() {
   const accent = isDark ? '#5C82A0' : '#355C7D'
 
   useEffect(() => {
-    const consent = localStorage.getItem('kw_cookie_consent_v4')
+    const consent = localStorage.getItem('kw_cookie_consent_v5')
     if (consent === 'accepted') {
       loadGoogleAnalytics()
     } else if (!consent) {
@@ -48,13 +48,13 @@ export default function CookieConsent() {
   }, [])
 
   function accept() {
-  localStorage.setItem('kw_cookie_consent_v4', 'accepted')
+  localStorage.setItem('kw_cookie_consent_v5', 'accepted')
     loadGoogleAnalytics()
     setShow(false)
   }
 
   function decline() {
-  localStorage.setItem('kw_cookie_consent_v4', 'declined')
+  localStorage.setItem('kw_cookie_consent_v5', 'declined')
     setShow(false)
   }
 
